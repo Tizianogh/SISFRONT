9806759860ecd7c6b5bbe0d40866579e2912ec64
Aïda Oukrif <aidaoukrif@gmail.com>
2022-03-22 17:16:20 +0100

    front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -22,7 +22,9 @@ import { ConnexionComponent } from './connexion/connexion.component';
     AppRoutingModule,
     PhonesModule,
     ComputerModule,
-    StockageModule
+    StockageModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule }  from '@angular/forms';
+import { NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-connexion',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connexion.component.css']
 })
 export class ConnexionComponent implements OnInit {
+  messageError:boolean=false;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+connexion(connexionForm: any){
+  let data = connexionForm.value;
+  console.log(connexionForm.value)
+}
 }
